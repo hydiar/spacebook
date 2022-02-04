aa796043b86994439ba5b5114050d0332a0a23de
harry <harry@desktop.localdomain>
2022-02-04 15:42:55 +0000

    login screen design finished

--- App.js
+++ App.js
@@ -8,7 +8,12 @@ import {
     Image,
     ImageBackground,
     TextInput,
+    Button,
+    TouchableOpacity,
+    Linking,
     Dimensions } from "react-native";
+import TextAncestorContext from "react-native-web/dist/exports/Text/TextAncestorContext";
+import button from "react-native-web/dist/exports/Button";
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -34,7 +39,7 @@ class App extends Component{
 
                   <TextInput
                       style={styles.input}
-                      placeholder={"Phone number or email address"}
+                      placeholder={"Email address"}
                       onChangeText={(email) => setEmail(email)}
                   />
 
@@ -45,12 +50,29 @@ class App extends Component{
                       onChangeText={(password) => setPassword(password)}
                   />
 
+                  <View style={{flexDirection: 'row'}}>
+                      <View>
+                          <TouchableOpacity activeOpacity={0.95} style={styles.buttonRegister}>
+                              <Text style={styles.buttonText}>Register</Text>
+                          </TouchableOpacity>
+                      </View>
+
+                      <View>
+                          <TouchableOpacity activeOpacity={0.95} style={styles.button}>
+                              <Text style={styles.buttonText}>Log In</Text>
+                          </TouchableOpacity>
+                      </View>
+                  </View>
+
+                  <View style={{height: windowHeight/14}}></View>
+
               </View>
           </ImageBackground>
 
       );
   }
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -64,14 +86,47 @@ const styles = StyleSheet.create({
         justifyContent: "center"
     },
     input: {
+        color: "white",
+        placeholderTextColor: "#bbb",
         borderColor: "transparent",
         borderBottomColor: "#aaa",
-        height: 40,
+        height: windowHeight / 22,
         width: windowWidth * 0.75,
-        margin: 12,
+        margin: 11,
         borderWidth: 1,
         //padding: 10,
     },
+    button: {
+        flexDirection: 'row',
+        height: windowHeight / 23,
+        width: windowWidth * 1 / 3,
+        backgroundColor: '#791AD9',
+        text: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 12,
+        marginHorizontal: (windowWidth * 1 / 3) / 8,
+        elevation: 10,
+        borderRadius: 4
+    },
+    buttonRegister: {
+        flexDirection: 'row',
+        height: windowHeight / 23,
+        width: windowWidth * 1 / 3,
+        backgroundColor: '#E03E69',
+        text: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 12,
+        marginHorizontal: (windowWidth * 1 / 3) / 8,
+        elevation: 10,
+        borderRadius: 4
+
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: windowWidth / 25
+    }
 });
 
 export default App
