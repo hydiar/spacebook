44a5698fd3cd0a4534321182c2d98b6709496523
harry <harry@desktop.localdomain>
2022-02-04 13:46:07 +0000

    logo added and rendered

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+'use strict';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react'
+import {
+  StyleSheet, Text, View, Image, Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+class App extends Component{
+  render() {
+      let logo_size = windowWidth / 2
+
+      if (windowHeight < windowWidth) {
+          logo_size = windowWidth / 5
+      }
+      return (
+          <View style={styles.container}>
+              <Image source={require("./assets/logo.png")}
+                     style={{width: logo_size, height: logo_size}}
+              />
+
+              <Text>
+                  {"hello, world"}
+              </Text>
+          </View>
+      );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
+
+export default App
