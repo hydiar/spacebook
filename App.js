62d47b561f6e6877a06249ec1dc556f8a4fa4aac
harry <harry@desktop.localdomain>
2022-03-02 12:40:55 +0000

    Homescreen and Login functional

--- App.js
+++ App.js
@@ -1,132 +1,31 @@
 'use strict';
 
-import React, { Component } from 'react'
-import {
-  StyleSheet,
-    Text,
-    View,
-    Image,
-    ImageBackground,
-    TextInput,
-    Button,
-    TouchableOpacity,
-    Linking,
-    Dimensions } from "react-native";
-import TextAncestorContext from "react-native-web/dist/exports/Text/TextAncestorContext";
-import button from "react-native-web/dist/exports/Button";
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-class App extends Component{
-  render() {
-      let logo_size = windowWidth / 2
+import HomeScreen from './components/home'
+import WelcomeScreen from './components/welcome'
+import NavBar from './components/navigationbar'
+import Register from './components/register'
+import WritePost from './components/writepost'
 
-      if (windowHeight < windowWidth) {
-          logo_size = windowWidth / 5
-      }
+const Stack = createNativeStackNavigator();
 
-      const [email, getEmail] = "";
-      const [password, getPassword] = "";
-
-      return (
-          <ImageBackground source={require("./assets/stars.png")} style={{width: windowWidth, height: windowHeight}}>
-              <View style={styles.container}>
-
-                  <Image source={require("./assets/logo.png")}
-                         style={{width: logo_size, height: logo_size}}
-                  />
-
-                  <TextInput
-                      style={styles.input}
-                      placeholder={"Email address"}
-                      onChangeText={(email) => setEmail(email)}
-                  />
-
-                  <TextInput
-                      style={styles.input}
-                      placeholder={"Password"}
-                      secureTextEntry={true}
-                      onChangeText={(password) => setPassword(password)}
-                  />
-
-                  <View style={{flexDirection: 'row'}}>
-                      <View>
-                          <TouchableOpacity activeOpacity={0.95} style={styles.buttonRegister}>
-                              <Text style={styles.buttonText}>Register</Text>
-                          </TouchableOpacity>
-                      </View>
-
-                      <View>
-                          <TouchableOpacity activeOpacity={0.95} style={styles.button}>
-                              <Text style={styles.buttonText}>Log In</Text>
-                          </TouchableOpacity>
-                      </View>
-                  </View>
-
-                  <View style={{height: windowHeight/14}}></View>
-
-              </View>
-          </ImageBackground>
-
-      );
-  }
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Welcome" component={WelcomeScreen} />
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="Write Post" component={WritePost} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        //backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    image: {
-        flex: 1,
-        justifyContent: "center"
-    },
-    input: {
-        color: "white",
-        placeholderTextColor: "#bbb",
-        borderColor: "transparent",
-        borderBottomColor: "#aaa",
-        height: windowHeight / 22,
-        width: windowWidth * 0.75,
-        margin: 11,
-        borderWidth: 1,
-        //padding: 10,
-    },
-    button: {
-        flexDirection: 'row',
-        height: windowHeight / 23,
-        width: windowWidth * 1 / 3,
-        backgroundColor: '#791AD9',
-        text: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 12,
-        marginHorizontal: (windowWidth * 1 / 3) / 8,
-        elevation: 10,
-        borderRadius: 4
-    },
-    buttonRegister: {
-        flexDirection: 'row',
-        height: windowHeight / 23,
-        width: windowWidth * 1 / 3,
-        backgroundColor: '#E03E69',
-        text: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 12,
-        marginHorizontal: (windowWidth * 1 / 3) / 8,
-        elevation: 10,
-        borderRadius: 4
-
-    },
-    buttonText: {
-        color: "#fff",
-        fontSize: windowWidth / 25
-    }
-});
-
-export default App
+export default App;
